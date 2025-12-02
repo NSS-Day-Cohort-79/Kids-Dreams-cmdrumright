90bbbbb964c3cb4d816091bb37ece1b4672f1fa8
cmdrumright <cdrumright@protonmail.com>
2025-12-02 09:09:01 -0600

    fixed spelling mistakes in database

--- scripts/database.js
+++ scripts/database.js
@@ -36,7 +36,7 @@ const database = {
         sport: "Baseball"
     }, {
         id: 8,
-        mane: "Tiffanie Tubby",
+        name: "Tiffanie Tubby",
         email: "ttubby7@intel.com",
         sport: "Basketball"
     }, {
@@ -69,7 +69,7 @@ const database = {
         id: 4,
         name: "Karine McKennan",
         celebrityId: 6,
-        wissh: "watching a landing on a super difficult vault!"
+        wish: "watching a landing on a super difficult vault!"
     }, {
         id: 5,
         name: "Cissy Weavers",
